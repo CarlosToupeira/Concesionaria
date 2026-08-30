8bd746a6c121f023e403270ff81901a960275a24
Ramiro <ramirocarlosc2323@gmail.com>
2026-08-29 23:01:49 -0300

    Feature/Refactor fetch to async/await request and add real-time car search filter

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,8 +1,16 @@
 const container = document.querySelector("#cars-grid");
+const searchInput = document.querySelector("#search-input");
 
-function renderizarAutos(carList) {
+let globalCarList = [];
+
+function renderCars(carList) {
     //Limpieza del contenedor div con el id #cars-grid
     container.innerHTML = "";
+
+    if (carList.lenght === 0) {
+        container.innerHTML = `<p style="color: #a0a6b1; grid-column: 1/-1;">No se encontraron vehículos que coincidan con tu búsqueda.</p>`;
+        return;
+    }
 
     carList.forEach(car => {
         // por cada auto de mi arreglo agarro los datos necesarios para crear tarjetas html al contenedor div
@@ -23,18 +31,32 @@ function renderizarAutos(carList) {
     });
 }
 
-//Petición fetch de manera que use Promesas (Cuando pueda obtener el archivo JSON genera una respuesta con los datos del JSON, sino genera errores.)
-fetch("js/cars.json")
-    .then(response => {
+// Carga de autos usando async / await
+async function loadCars() {
+    try {
+        const response = await fetch("js/cars.json");
         if (!response.ok) {
-            throw new Error("Error al cargar el archivo JSON");
+            throw new Error("No se puede obtener el archivo JSON");
         }
-        return response.json(); // Retorna la respuesta de texto JSON a un array de JS
-    })
-    .then(data => {
-        console.log("Datos cargados dinámicamente desde el JSON:", data);
-        renderizarAutos(data) // llamo la funcion con los datos obtenídos correctamente del JSON
-    })
-    .catch(error => {
-        console.error("Hubo un problema con la petición Fetch:", error);
-    });
+        globalCarList = await response.json() // Espera que el JSON se convierta a objeto y guarda los datos en globalCarList || Fix error eliminé const porque no dejaba filtrar la lista a marcas o modelos específicos ademas de ser un arreglo global.
+        renderCars(globalCarList);
+    } catch (error) {
+        console.error("Hubo un problema al cargar los autos:", error);
+    }
+}
+
+//Evento de búsqueda en tiempo real
+searchInput.addEventListener("input", (event) => {
+    const searchText = event.target.value.toLowerCase().trim();
+
+    //filtro el arreglo global
+    const carFilter = globalCarList.filter(car =>
+        car.name.toLowerCase().includes(searchText)
+    );
+
+    //se renderiza solo tarjetas que pasaron por el filtro
+    renderCars(carFilter);
+});
+
+// LLamada a la función
+loadCars();
